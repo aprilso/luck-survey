4b71a382f3d0c50cbde64113538303477ef1488d
Laurie Waller <lauriewaller7@gmail.com>
2021-02-02 20:41:23 -0800

    write pseudo code for jquery
    Co-authored-by: April Soetarman <aprilsoetarman@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,13 @@ $(document).ready(function(){
   $("form#luckSurvey").submit(function(event) {
     event.preventDefault();
     $("luck-responses").show();
-    
+
 
   }
   
-  
-  
 
 
-
-
-  //
+  //two ways to return fortunes. 1) return an individual response for each bad luck - ex: saw a black cat, draw a circle of salt. 
+  //2. if, else if, else statement. if sum of unlucky > lucky, bad fortune. sum of unlucky = sum of lucky, precarious month. sum of lucky > unlucky, you're doing well
+  // Using a loop (for? each?) to total up the sum of something checked in "lucky" name attribute, then comparing.
 });

84d916761f39cde5fdfa16bac70f5c804eed31cb
Laurie Waller <lauriewaller7@gmail.com>
2021-02-02 21:11:10 -0800

    update README
    Co-authored-by: April Soetarman <aprilsoetarman@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,5 @@ $(document).ready(function(){
   //two ways to return fortunes. 1) return an individual response for each bad luck - ex: saw a black cat, draw a circle of salt. 
   //2. if, else if, else statement. if sum of unlucky > lucky, bad fortune. sum of unlucky = sum of lucky, precarious month. sum of lucky > unlucky, you're doing well
   // Using a loop (for? each?) to total up the sum of something checked in "lucky" name attribute, then comparing.
+  // Can also use a score system and return a different fortune for each score range
 });
